4dea0b0e3f88260ed6e21985b7718231a2608c24
Rodrigo Andrade <rodriggpda@gmail.com>
2021-06-05 11:53:35 -0300

    renew script

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,66 @@
-$(document).ready(function() {
+async function boot() {
+  let csv_link = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSaTp-aYU6dmV0TX6laiFZq0j0h3wFoTYyl1PkakuZgChxkoDwpETWwVhsms-TTELxWF-X_ORF63cfy/pub?output=csv";
 
-  let csv_link = "  "; // colar aqui
-  let t_header = $(".t-header");
-  let t_body = $(".t-body");
-
-  $.ajax({
+  await $.ajax({
     url: csv_link,
   })
   .done(function(data) {
 
-    let arr = data.split(/\r\n/);
+    //\d{1}\,\d{3} teste 5,194 para 5.194
+    
+    data = data.split(/\r\n/);
+    
+    var lines = [];
+    var col = [];
+    var res = [];
 
-    let ct_body;
-
-    for (let i = 0; i < arr.length; i++) {
-
-      // console.log(i);
-
-      let col = arr[i].split(",");
-
-      if(i === 0 && i <= col.length) {
-        
-        for(let c = 0; c < col.length; c++) {
-          t_header.append(`<th>${col[c]}</th>`);
-        }
-      } else {
-        ct_body += '<tr>';
-
-        for(let c = 0; c < col.length; c ++) {
-          ct_body += `<td> ${col[c]} </td>`;
-        }
-
-        ct_body += '</tr>';
-      }
-
-      //console.log("final");
-      
+    for(let i = 0; i < data.length; i++) {
+      lines.push(data[i].split((/([a-z][A-Z]{*})\,/)));
     }
 
-    t_body.append(ct_body);
+    lines.splice(0, 1);
+
+    for(let i = 0; i < lines.length; i++) {
+      col.push(lines[i])
+      res.push({ "value": i, "data": col[i][0].split(/[a-zA-Z]{0}\,/)})
+    }
+
+    var cities = document.querySelector("#cities");
+    var consume = document.querySelector("#consume");
+    var calcButton =  document.getElementById("calc");
+    var city = [];
+    
+
+    for(let i = 0; i < res.length; i++) {
+      city.push(document.createElement("option"));
+      city[i].setAttribute("value", res[i].value)
+      city[i].innerHTML = res[i].data[0]
+      cities.append(city[i])
+    }
+
+    var rad;
+    var radValue = document.getElementById("rad");
+    var pkit = document.getElementById("pkit");
+
+    cities.addEventListener("change", function() {
+      rad = res[cities.value].data[2]+","+res[cities.value].data[3];
+      radValue.value = rad.replace(/"/g, "");
+    });
+
+    calcButton.addEventListener("click", function() {
+      // CONSUMO DA CONTA/30/MÉDIA ANUAL DE IRRADIAÇÃO DA CIDADE/0,75
+      let rad = radValue.value.replace(/\,/g, ".");						
+      let potency = consume.value / 30 / rad / 0.75;
+      pkit.innerHTML = potency.toFixed(2);
+    });
+
+    return;
 
   });
+}
+
+$(document).ready(function() {
+
+  boot();
 
 });
